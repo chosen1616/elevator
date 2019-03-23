c9c26cb29a7daa5c669d667fd5799963920f9698
caihao1616 <haocai@usc.edu>
2019-03-22 22:40:37 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'elevator';
+export class AppComponent implements OnInit {
+  totalFloor = 5;
+  currFloor = 3;
+  floors = [];
+
+  ngOnInit() {
+    for (let i = this.totalFloor; i > 0; i--) {
+      this.floors.push(i);
+    }
+  }
 }
